73e96ceb327245657b1b7066971c51ca55067368
Franco Aguilera <fxaaguilera@uamv.edu.ni>
2025-10-30 17:55:36 -0600

    Se agrega el OpenXava completo

--- facturacion/src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
+++ facturacion/src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -15,4 +15,8 @@ public class Cliente {
     @Required  // Se mostrará un error de validación si la propiedad nombre se deja en blanco
     String nombre;
 
+    @Embedded // Así para referenciar a una clase incrustable
+    Direccion direccion; // Una referencia Java convencional
+
+
 }
